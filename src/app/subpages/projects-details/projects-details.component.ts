13763508e65881aea82ba32ed3933d069055fcb7
BryantMagik <bryanedwardse@gmail.com>
2024-11-07 12:12:38 +0100

    detalles mejorado

--- src/app/subpages/projects-details/projects-details.component.ts
+++ src/app/subpages/projects-details/projects-details.component.ts
@@ -3,16 +3,18 @@ import { ActivatedRoute } from '@angular/router';
 import { Project } from '../../model/project.interface';
 import { ProjectService } from '../../service/project/project.service';
 import { tap } from 'rxjs';
-import { FormBuilder,FormGroup,FormsModule,ReactiveFormsModule,Validators } from '@angular/forms';
-import { NgClass,NgFor,CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgClass, NgFor, CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { PROYECTOSTATUS, PROYECTOTYPE } from '../../types/models';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { FloatLabelModule } from 'primeng/floatlabel';
 
 
 @Component({
   selector: 'app-projects-details',
   standalone: true,
-  imports: [NgClass,NgFor,CommonModule,FormsModule,ReactiveFormsModule],
+  imports: [NgClass, NgFor, CommonModule, FormsModule, ReactiveFormsModule,MultiSelectModule,FloatLabelModule],
   templateUrl: './projects-details.component.html',
   styleUrls: ['./projects-details.component.css']
 })
@@ -26,22 +28,22 @@ export class ProjectsDetailsComponent implements OnInit {
 
   projectTypes = Object.values(PROYECTOTYPE)
   projectStatuses = Object.values(PROYECTOSTATUS)
-  availableParticipants: string[] = []
+  availableParticipants: { id: string; first_name: string }[] = [];
 
   constructor(
     private route: ActivatedRoute,
     private projectService: ProjectService,
     private fb: FormBuilder,
     private router: Router
-  ) { 
+  ) {
     this.projectsFormular = this.fb.group({
       id: [{ value: '', disabled: true }],
-      name: [{value: ''}, [Validators.required, Validators.maxLength(50)]],
-      description: [{value: ''}],
-      code: [{value: ''}, [Validators.required, Validators.maxLength(10)]],
-      type: [{value: ''}, Validators.required],
-      status: [{value: ''}, Validators.required],
-      userId: [{value: ''}, Validators.required],
+      name: [{ value: '' }, [Validators.required, Validators.maxLength(50)]],
+      description: [{ value: '' }],
+      code: [{ value: '' }, [Validators.required, Validators.maxLength(10)]],
+      type: [{ value: '' }, Validators.required],
+      status: [{ value: '' }, Validators.required],
+      userId: [{ value: '' }, Validators.required],
       participants: [[]]
     });
   }
@@ -54,6 +56,7 @@ export class ProjectsDetailsComponent implements OnInit {
         this.getProjectById(this.projectId)
       }
     })
+
   }
 
   navigateToProjects() {
@@ -65,7 +68,7 @@ export class ProjectsDetailsComponent implements OnInit {
       tap({
         next: (project: Project | null) => {
           this.project = project;
-          console.log("Subiendo",this.project)
+          console.log("Subiendo", this.project)
           if (project) {
             this.projectsFormular.patchValue({
               id: project.id,
@@ -75,8 +78,9 @@ export class ProjectsDetailsComponent implements OnInit {
               type: project.type,
               status: project.status,
               userId: project.author?.first_name,
-              participants: project.participants || []
+              participants: project.participants.map(participant => participant.id)
             });
+            this.availableParticipants = project.participants.map(participant => ({ id: participant.id, first_name: participant.first_name }));
           }
         },
         error: () => {
