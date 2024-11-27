dabab8f05127e820c23788ac5b54951f557760d3
HFPRUZ <heflaruz@gmail.com>
2024-11-27 11:54:39 +0100

    layout_carga_usuario_pe

--- src/app/pages/layout/layout.component.ts
+++ src/app/pages/layout/layout.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from '../../shared/navbar/navbar.component';
 import { CustomSidebarComponent } from '../../shared/custom-sidebar/custom-sidebar.component';
+import { UserService } from '../../service/user/user.service';
+import { User } from '../../model/user.interface';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-layout',
@@ -14,9 +17,30 @@ export class LayoutComponent implements OnInit {
 
   @ViewChild(NavbarComponent) navbarComponent!: NavbarComponent;
 
-  constructor() { }
+  loading = true;
+  error: string | null = null;
+  user: User | null = null;
+
+  constructor(
+    private userService: UserService
+  ) { }
 
   ngOnInit() {
+    this.loadUserProfile();
+  }
 
+  private loadUserProfile(): void {
+    this.userService.loadUserProfile().pipe(
+      tap({
+        next: (user: User | null) => {
+          this.user = user;
+          if (!user) {
+            this.error = 'Failed to load user profile';
+          }
+        },
+        error: () => this.error = 'Failed to load user profile',
+        complete: () => this.loading = false,
+      })
+    ).subscribe();
   }
 }
