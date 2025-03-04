170978e8eaba949ddbd5a7962aa0fac7d0bd1a72
HFPRUZ <heflaruz@gmail.com>
2025-03-04 19:35:12 +0100

    feat: enhance my-task component with workspace selection and loading functionality

--- src/app/pages/my-task/my-task.component.ts
+++ src/app/pages/my-task/my-task.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
-
+import { Component,EventEmitter,OnInit,Output } from '@angular/core';
+import { Workspace } from '../../model/workspace.interface';
+import { WorkspaceService } from '../../service/workspace/workspace.service';
+import { tap } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-my-task',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './my-task.component.html',
   styleUrl: './my-task.component.css'
 })
-export class MyTaskComponent {
+export class MyTaskComponent implements OnInit {
+  workspaceId: string | null = null;
+  selectedWorkspace: Workspace | null = null;
+
+  constructor(
+    private route: ActivatedRoute,
+    private workspaceService: WorkspaceService
+  ) {}
+
+  ngOnInit(): void {
+    this.route.parent?.paramMap.subscribe(params => {
+      this.workspaceId = params.get('workspaceId');
+      if (this.workspaceId) {
+        this.loadWorkspace();
+      }
+    });
+    
+  }
+
+  private loadWorkspace(): void {
+    if (!this.workspaceId) return;
+
+    this.workspaceService.getWorkspaceById(this.workspaceId).subscribe({
+      next: (workspace) => {
+        this.selectedWorkspace = workspace;
+      },
+      error: () => {
+        console.error('Error loading workspace');
+      }
+    });
+  }
 
 }
